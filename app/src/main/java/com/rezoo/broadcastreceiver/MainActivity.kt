bb5c975c79bbe84355647ad5ceeba826a576270d
reza <rezasalehibabarsad@hotmail.com>
2023-05-19 18:05:40 +0330

    final2

--- app/src/main/java/com/rezoo/broadcastreceiver/MainActivity.kt
+++ app/src/main/java/com/rezoo/broadcastreceiver/MainActivity.kt
@@ -10,7 +10,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         receiver = AirPlaneChangedReceiver()
+
         IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED).also {
             registerReceiver(receiver,it)
         }
